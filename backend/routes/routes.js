4589c269bc4fcbcbfb00bef5593e7fa460ef302b
Anish <anishkakaiya@outlook.com>
2022-06-28 14:56:32 +0100

    add route for single workout using params

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,9 +2,17 @@ const express = require('express')
 
 const router = express.Router()
 
+
+//GET all workouts
 router.get('/', (req, res) => {
   res.json({message: 'GET all workouts'})
 })
+
+//GET single workout
+router.get('/:id', (req, res) => {
+  res.json({message: 'GET a single workout'})
+})
+
 
 
 
