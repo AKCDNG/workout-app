6325be50e0920e6bc6f0195375d292ec25b5c298
Anish <anishkakaiya@outlook.com>
2022-06-28 14:59:08 +0100

    add post, delete and update routes boilerplated with JSON responses

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -13,7 +13,20 @@ router.get('/:id', (req, res) => {
   res.json({message: 'GET a single workout'})
 })
 
+//POST a new workout
+router.post('/', (req, res) => {
+  res.json({message: 'POST a new workout'})
+})
 
+//DELETE a workout
+router.delete('/:id', (req, res) => {
+  res.json({message: 'DELETE a workout'})
+})
+
+//UPDATE a workout
+router.patch('/:id', (req, res) => {
+  res.json({message: 'UPDATE a workout'})
+})
 
 
 module.exports = router;
