c297f6dedd474e87583d267f5954c080cd666b9c
Anish <anishkakaiya@outlook.com>
2022-07-03 19:02:45 +0100

    add navbar to html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages & Components
 import Home from './pages/home'
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route
