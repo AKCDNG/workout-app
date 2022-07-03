22b119fe7b42205401df80b4d1611983c968de5d
Anish <anishkakaiya@outlook.com>
2022-07-03 20:05:46 +0100

    response check error handler added

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -4,9 +4,33 @@ const WorkoutForm = () => {
   const [title, setTitle] = useState('')
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const workout = {title, load, reps}
+
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const json = await response.json()
+
+    if(!response.ok) {
+      setError(json.error)
+    }
+    if(response.ok) {
+      setError(null)
+      console.log("New Workout Added", json)
+    }
+  }
 
   return (
-    <form className="create">
+    <form className="create" onSubmit={handleSubmit}>
       <h3>Add A New Workout</h3>
 
       <label>Exercise Title:</label>
