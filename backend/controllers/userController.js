207d83bbb2b8fb31825f0bb2bca64ba6698380b0
Anish <anishkakaiya@outlook.com>
2022-07-20 23:02:52 +0100

    fix type for require user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,21 @@
-const user = require('../models/userModel')
+const User = require('../models/userModel');
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({message: 'login user'})
-}
+  res.json({ message: 'login user' });
+};
 
 // sign up user
 const signUpUser = async (req, res) => {
-  res.json({message: 'signup user'})
-}
+  const { email, password } = req.body;
 
-module.exports = {loginUser, signUpUser}
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signUpUser };
