f287cd2a7a5a8df1bd580da281bb93aa237905e5
Anish <anishkakaiya@outlook.com>
2022-07-20 23:03:11 +0100

    add bcrypt to static function

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -14,14 +15,19 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.statics.signup = async (email, password) => {
+userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email });
 
-  if(exists) {
-    throw Error('Email already in use.')
+  if (exists) {
+    throw Error('Email already in use.');
   }
 
-  
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
 };
 
 module.exports = mongoose.model('User', userSchema);
