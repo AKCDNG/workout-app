33b7b20fe54eb11faef9f90a004e4a4a272231f7
Anish <anishkakaiya@outlook.com>
2022-07-21 15:23:35 +0100

    add auth context component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/workoutsContext'
+import { WorkoutsContextProvider } from './context/workoutsContext';
+import { authContextProvider } from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <authContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </authContextProvider>
   </React.StrictMode>
 );
